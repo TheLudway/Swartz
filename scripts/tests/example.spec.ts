ae64b38a1b3ce97a31938ca8dbb7e764b65510e4
TheLudway <theludway@gmail.com>
2026-05-11 23:00:26 -0500

    Obtain the top 5 torrents by seeders

--- scripts/tests/example.spec.ts
+++ scripts/tests/example.spec.ts
@@ -18,7 +18,35 @@ test('search from telegram input', async ({ page }) => {
         state: 'visible'
     });
 
+    await page.getByText('S', { exact: true }).click();
+    await page.locator('th[title="Сиды"][aria-sort="descending"]').waitFor();
+    
+    const rows = await page.locator('tr[role="row"]');
+    const results = [];
+
+    for (let i = 1; i <= Math.min(await rows.count(), 5); i++){
+        const row = rows.nth(i).locator('td');
+
+        const title = await row.nth(3).textContent();
+        const size = await row.nth(5).textContent();
+        const seeds = await row.nth(6).textContent();
+        const downloads = await row.nth(8).textContent();
+        
+        results.push({ 
+            title: title ? title.trim().replace(/\s+/g, ' ') : null, 
+            size, 
+            seeds, 
+            downloads: downloads ? downloads.trim() : null 
+        });
+    }
+    
+    console.log('EXTRACTED_RESULTS:' + JSON.stringify(results));
+
+
+
     await page.screenshot({ path: 'screenshot.png', fullPage: true }); 
 });
+
+
 
 
