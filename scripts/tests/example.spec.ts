c35f7fbd22a5876e2fc3df3520a96f809abb06c8
TheLudway <theludway@gmail.com>
2026-05-11 20:39:20 -0500

    Automation to do the search

--- scripts/tests/example.spec.ts
+++ scripts/tests/example.spec.ts
@@ -1,18 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const query = process.env.SEARCH_QUERY || '';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('search from telegram input', async ({ page }) => {
+    if (!query){
+        throw new Error('No query provided');
+    }
+
+    await page.goto('https://rutracker.org/', {
+        waitUntil: 'domcontentloaded'
+    });
+
+    await page.fill('#search-text', query);
+    await page.locator('#search-submit').click();
+
+    await page.waitForSelector('#tor-tbl tbody tr', {
+        state: 'visible'
+    });
+
+    await page.screenshot({ path: 'screenshot.png', fullPage: true }); 
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
